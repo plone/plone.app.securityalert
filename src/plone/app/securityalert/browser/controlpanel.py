2afa952195ba19eafc294e93dcf79a3d27003467
Ken Wasetis <ken.wasetis@contextualcorp.com>
2011-11-07 19:45:11 -0600

    Configlet in functional, but useless state.

--- src/plone/app/securityalert/browser/controlpanel.py
+++ src/plone/app/securityalert/browser/controlpanel.py
@@ -2,7 +2,12 @@ from plone.app.registry.browser import controlpanel
 
 from plone.app.securityalert.interfaces import ISecurityAlertSettings, _
 
+from Acquisition import aq_base, aq_inner
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter, queryUtility
+from plone.registry.interfaces import IRegistry
+from plone.registry.interfaces import IRecordModifiedEvent
 
 class SecurityAlertSettingsEditForm(controlpanel.RegistryEditForm):
     """Security Alert settings form.
@@ -15,23 +20,40 @@ class SecurityAlertSettingsEditForm(controlpanel.RegistryEditForm):
                     default=u"The Security Alert tool reports the "
                              "last time run and provides configuration options.")
 
+    def updateFields(self):
+        super(SecurityAlertSettingsEditForm, self).updateFields()
+
+    def updateWidgets(self):
+        super(SecurityAlertSettingsEditForm, self).updateWidgets()
+
 
 class SecurityAlertSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
     form = SecurityAlertSettingsEditForm
-
     index = ViewPageTemplateFile('controlpanel.pt')
+    last_time_run = 'This service has not yet run.'
 
     def settings(self):
         """Compose a string that contains all registry settings that are
-           needed for the security alert control panel.
+           needed for the discussion control panel.
         """
         registry = queryUtility(IRegistry)
         settings = registry.forInterface(ISecurityAlertSettings, check=False)
         output = []
 
         # Globally enabled
-        #if settings.last_time_run:
-        #    output.append("last_time_run")
+        if settings.last_time_run:
+            output.append("last_time_run")
 
-        settings.last_time_run = 'The security alert service has not yet been run.'
-        output.append("last_time_run")
+
+    def mailhost_warning(self):
+        """Returns true if mailhost is not configured properly.
+        """
+        # Copied from plone.app.controlpanel/plone/app/controlpanel/overview.py
+        mailhost = getToolByName(aq_inner(self.context), 'MailHost', None)
+        if mailhost is None:
+            return True
+        mailhost = getattr(aq_base(mailhost), 'smtp_host', None)
+        email = getattr(aq_inner(self.context), 'email_from_address', None)
+        if mailhost and email:
+            return False
+        return True
